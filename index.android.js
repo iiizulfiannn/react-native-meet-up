2b4e09e722a779d1196122c3d7bf2bbb61d40e5f
iiizulfiannn <60847052+iiizulfiannn@users.noreply.github.com>
2021-12-06 10:55:27 +0700

    add automation

--- index.android.js
+++ index.android.js
@@ -2,20 +2,18 @@
  * @providesModule JitsiMeet
  */
 
-import { NativeModules, requireNativeComponent } from 'react-native';
+import { NativeModules, requireNativeComponent } from "react-native";
 
-export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
-export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
-const call = JitsiMeetModule.call;
-const audioCall = JitsiMeetModule.audioCall;
-JitsiMeetModule.call = (url, userInfo) => {
+export const MeetUpView = requireNativeComponent("RNJitsiMeetView");
+export const MeetUpModule = NativeModules.RNJitsiMeetModule;
+const call = MeetUpModule.call;
+const audioCall = MeetUpModule.audioCall;
+MeetUpModule.call = (url, userInfo) => {
   userInfo = userInfo || {};
   call(url, userInfo);
-}
-JitsiMeetModule.audioCall = (url, userInfo) => {
+};
+MeetUpModule.audioCall = (url, userInfo) => {
   userInfo = userInfo || {};
   audioCall(url, userInfo);
-}
-export default JitsiMeetModule;
-
-
+};
+export default MeetUpModule;
